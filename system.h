f3552c47addfac4da433551e7707b4375aed8866
saikatbsk <saikatbsk@gmail.com>
2015-06-01 05:47:17 +0530

    Tabstop changed to 4

--- system.h
+++ system.h
@@ -2,11 +2,11 @@
 #define _SYSTEM_H
 
 extern unsigned char * memcpy(unsigned char * dest,
-    const unsigned char * src, int count);
+        const unsigned char * src, int count);
 extern unsigned char * memset(unsigned char * dest,
-    unsigned char val, int count);
+        unsigned char val, int count);
 extern unsigned short * memsetw(unsigned short * dest,
-    unsigned short val, int count);
+        unsigned short val, int count);
 extern int strlen(const char * str);
 extern unsigned char inportb(unsigned short _port);
 extern void outportb(unsigned short _port, unsigned char _data);
