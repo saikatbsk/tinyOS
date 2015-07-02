ea5755700d58c0a495af708c9a5b36fc8b5a5235
saikatbsk <saikatbsk@gmail.com>
2015-07-02 12:37:14 +0530

    Review

--- main.c
+++ main.c
@@ -4,24 +4,30 @@
 unsigned char * memcpy(unsigned char * dest,
         const unsigned char * src, int count) {
     int i;
+
     for(i = 0 ; i < count ; ++i)
         dest[i] = src[i];
+
     return dest;
 }
 
 extern unsigned char * memset(unsigned char * dest,
         unsigned char val, int count) {
     int i;
+
     for(i = 0 ; i < count ; ++i)
-        dest[i]=val;
+        dest[i] = val;
+
     return dest;
 }
 
 extern unsigned short * memsetw(unsigned short * dest,
         unsigned short val, int count) {
     int i;
+
     for(i = 0 ; i < count ; ++i)
-        dest[i]=val;
+        dest[i] = val;
+
     return dest;
 }
 
@@ -35,6 +41,7 @@ int strlen(const char * str) {
 unsigned char inportb(unsigned short _port) {
     unsigned char rv;
     __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
+
     return rv;
 }
 
